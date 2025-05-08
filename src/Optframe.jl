f5b7bcb80715888368ab2194368622cc103716fb
fellipessanha <fellipe900@gmail.com>
2025-05-08 18:54:43 -0300

    feat: add ccall by symbol in Opframe.jl

--- src/Optframe.jl
+++ src/Optframe.jl
@@ -1,37 +1,47 @@
 module OptFrame
 
+using Libdl
 export Engine, init_engine, welcome
 
 mutable struct Engine
-        gc_arena::IdDict{Ptr{Cvoid},Any}
-        ll_int::Int
-        hf::Ptr{Cvoid}
+    gc_arena::IdDict{Ptr{Cvoid},Any}
+    ll_int::Int
+    hf::Ptr{Cvoid}
 end
-
-# =========================
 
 const libpath = "./optframe_lib.so"
 
+const optframe_ptr = Libdl.dlopen(libpath)
+
+function get_function_symbol(module_pointer::Ptr{Nothing}, function_name::String)
+    return Libdl.dlsym(module_pointer, function_name)
+end
+
+function create_engine_by_symbol(n::Int32)
+    creation_symbol = get_function_symbol(optframe_ptr, "optframe_api1d_create_engine")
+    return @ccall $creation_symbol(n::Cint)::Ptr{Cvoid}
+end
+
 function optframe_api1d_create_engine(n::Int32)
-        @ccall libpath.optframe_api1d_create_engine(n::Cint)::Ptr{Cvoid}
+    return @ccall libpath.optframe_api1d_create_engine(n::Cint)::Ptr{Cvoid}
 end
 
 function optframe_api0d_engine_welcome(eng::Ptr{Cvoid})
-        @ccall libpath.optframe_api0d_engine_welcome(eng::Ptr{Cvoid})::Cvoid
+    return @ccall libpath.optframe_api0d_engine_welcome(eng::Ptr{Cvoid})::Cvoid
 end
 
 function init_engine(ll_int::Int64)::Engine
-        init_engine(Int32(ll_int))
+    return init_engine(Int32(ll_int))
 end
 
 function init_engine(ll_int::Int32)::Engine
-        e = Engine(IdDict{Ptr{Cvoid},Any}(), ll_int, optframe_api1d_create_engine(ll_int))
-        optframe_api0d_engine_welcome(e.hf)
-        return e
+    e = Engine(IdDict{Ptr{Cvoid},Any}(), ll_int, optframe_api1d_create_engine(ll_int))
+    optframe_api0d_engine_welcome(e.hf)
+    return e
 end
 
 function welcome(e::Engine)
-        optframe_api0d_engine_welcome(e.hf)
+    optframe_api0d_engine_welcome(e.hf)
 end
 
 end # module Optframe
