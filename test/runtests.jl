7783cef178454ab0d84ced396107409e8f8b8754
Pedro Maciel Xavier <mail@pedro.xn--qxam>
2025-05-13 23:12:50 -0400

    Make test report verbose

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,10 @@ using OptFrame: TSP
 include("load_tsp.jl")
 
 function main()
-    @testset "OptFrame.jl Test Suite" begin
+    @testset "OptFrame.jl Test Suite" verbose = true begin
         @test true
 
-        @testset "TSP" begin
+        @testset "TSP" verbose = true begin
             load_tsp()
         end
     end
