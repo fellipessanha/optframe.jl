1af9e6e87c0d03e0214af79ed92a9d22205caafe
Pedro Maciel Xavier <mail@pedro.xn--qxam>
2025-05-13 22:00:17 -0400

    Create tsp submodule

--- src/OptFrame.jl
+++ src/OptFrame.jl
@@ -448,4 +448,6 @@ function welcome(e::Engine)
     optframe_api0d_engine_welcome(e.hf)
 end
 
+include("tsp/tsp.jl")
+
 end # module Optframe
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,16 @@
 using Test
 using OptFrame
+using OptFrame: TSP
+
+include("load_tsp.jl")
 
 function main()
     @testset "OptFrame.jl Test Suite" begin
         @test true
+
+        @testset "TSP" begin
+            load_tsp()
+        end
     end
 
     return nothing
